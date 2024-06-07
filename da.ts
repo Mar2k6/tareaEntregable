d991f2789ebd53fba35dbc645bb648d094caf27e
mariano <mariano_2k6@hotmail.com>
2024-06-07 16:53:53 -0300

    tarea entregable bubblesort

--- da.ts
+++
@@ -1,9 +0,0 @@
-function parImpar(numero: number): string {
-    if (numero % 2 === 0) {
-      return 'par';
-    } else {
-      return 'impar';
-      }
-      }
-      console.log(parImpar(10)); // Output: 'par'
-console.log(parImpar(15)); // Output: 'impar'
